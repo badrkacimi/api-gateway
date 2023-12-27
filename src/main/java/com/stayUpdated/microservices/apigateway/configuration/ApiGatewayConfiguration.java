ea3330de28a9acdf9567690bb474b68975b75207
badrkacimi <badrvkacimi@gmail.com>
2023-12-27 12:22:35 +0100

    add gateway configuration

---
+++ src/main/java/com/stayUpdated/microservices/apigateway/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,27 @@
+package com.stayUpdated.microservices.apigateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+              .route(r -> r.path("/get")
+                .filters(f ->f.addRequestHeader("myHeader1", "application/json")
+                             .addRequestHeader("myHeader2", "application/xml"))
+                      .uri("http://httpbin.org:80"))
+                .route(r -> r.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(r -> r.path("/currency-conversion-new/**")
+                        .filters(f ->f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+                                        "/currency-conversion/feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+              .build();
+    }
+
+}
